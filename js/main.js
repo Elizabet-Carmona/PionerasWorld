d7150e22a1dd85163b22dbc233d953e1a1afb3ea
Elizabet-Carmona <e.ortiz@pascualbravo.edu.co>
2019-06-10 17:31:21 -0500

    .

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
-$("label span").click(function(){
-    $(".wrap").toggleClass("checked");
-  });
+let paises = [];
+
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', './paises.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);
+ }
+
+function llenarPaises() {
+    init();
+    const x = document.getElementById("mySelect");
+    for (let pais of paises) {
+        const option = document.createElement("option");
+        option.text = pais.name;
+        option.value = pais.name;
+        x.add(option);
+    }
+}
+
+function init() {
+    loadJSON(function(response) {
+     // Parse JSON string into object
+       paises = JSON.parse(response);
+    });
+}
+
+function login() {
+    document.getElementById("container").style.display = "block";
+  }
+
+function show() {
+    document.getElementById("pop-up").style.display = "block";
+  }
+
+function hide() {
+    document.getElementById("pop-up").style.display = "none";
+}
+
+function cambiarAvatar( parte , value){
+    attrributeHTML = 'avatar-' + parte;
+    document.querySelector('.your-avatar').setAttribute( attrributeHTML , value );
+}
